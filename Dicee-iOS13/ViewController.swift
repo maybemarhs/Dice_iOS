de629d96f5410da87c8c0e7f2de0345998062205
Maybemar Hernandez <maybemar.hernandez@gmail.com>
2023-07-10 17:49:40 -0500

    Added Initialization state code and button first action

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageView1.image = UIImage(named: "DiceSix")
+        diceImageView2.image = UIImage(named: "DiceTwo")
+        
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+    
+        diceImageView1.image = UIImage(named: "DiceFour")
+        diceImageView2.image = UIImage(named: "DiceFour")
+    }
+    
 }
 
